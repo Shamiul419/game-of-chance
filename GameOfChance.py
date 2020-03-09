f741b757e7e67895aea240cd4e0dc2693aa51952
Zarif Ahnaf <zarifahnaf@outlook.com>
2020-03-10 01:35:20 +0600

    2nd commit

--- GameOfChance.py
+++ GameOfChance.py
@@ -1,15 +1,19 @@
 import random
 
 money = 100
-
+new_money=0
 
 def flip_function(guess,bet):
     num = random.randint(1,2)
     if num == 1 and guess == "Heads":
+        print(money+bet)
         return "Heads"
     elif num != 1 and guess == "Tails":
+        print(money+bet)
         return "Tails"
-    else:
-        return "You have failed."
 
-print(flip_function("Heads",1))
+    else:
+        print(money-bet)
+        return "You have failed."
+        
+print(flip_function("Heads",2))
